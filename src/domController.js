f203bbe153a9c0028e57fd3045e7445dfda66189
ritz-w <ritz.wu@outlook.com>
2018-09-06 18:13:41 +0100

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,28 @@
 function updateDOM() {
+  const row1 = [...document.querySelectorAll('[id$="-0"]')]
+  const row2 = [...document.querySelectorAll('[id$="-1"]')]
+  const row4 = [...document.querySelectorAll('[id$="-3"]')]
+  const row5 = [...document.querySelectorAll('[id$="-4"]')]
+  const row3 = [...document.querySelectorAll('[id$="-2"]')]
 
+  for (let i=0; i<5; i++){
+    if (identicon.row15[i]%2 === 0) {
+      row1[i].style.backgroundColor = "000"
+      row5[i].style.backgroundColor = "000"
+    }
+  }
+
+  for (let i=0; i<5; i++){
+    if (identicon.row24[i]%2 === 0) {
+      row2[i].style.backgroundColor = "000"
+      row4[i].style.backgroundColor = "000"
+    }
+  }
+
+  for (let i=0; i<5; i++){
+    if (identicon.row3[i]%2 === 0) {
+      row3[i].style.backgroundColor = "000"
+    }
+  }
+  // row1.forEach(node => node.style.backgroundColor)
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 function handleSubmit(e) {
-
+  const allBlocks = [...document.querySelectorAll('span')]
+  allBlocks.forEach(block => block.style.backgroundColor = "")
+  const userInput = document.querySelectorAll('input')[0]
+  e.preventDefault()
+  const identicon = new Identicon(userInput.value)
+  window.identicon = identicon
+  updateDOM()
 }
 
 document.addEventListener("DOMContentLoaded", () => {
